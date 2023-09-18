382a95b3e93ca9ca3f80954f65af863503f3d601
Alexey Komissarov <al-ar-ol@yandex.ru>
2023-09-18 20:39:15 +0300

    fourth commit

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -96,7 +96,7 @@ class CommentViewSet(viewsets.ModelViewSet):
             serializer (Serializer): Экземпляр сериализатора.
         """
         serializer.save(author=self.request.user,
-                        post_id=int(self.kwargs.get('post_id')))
+                        post_id=self.kwargs.get('post_id'),)
 
     def perform_update(self, serializer):
         """
@@ -112,7 +112,7 @@ class CommentViewSet(viewsets.ModelViewSet):
         if serializer.instance.author != self.request.user:
             raise PermissionDenied('Изменение чужого контента запрещено!')
         serializer.save(author=self.request.user,
-                        post_id=int(self.kwargs.get('post_id')))
+                        post_id=self.kwargs.get('post_id'),)
 
     def perform_destroy(self, instance):
         """
